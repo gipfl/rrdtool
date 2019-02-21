e0bfb82ef3185b5a57bba57f404012c8fc657638
Thomas Gelf <thomas@gelf.net>
2019-02-21 19:13:29 +0100

    ErrorImage: new rendering helper

---
+++ src/Rendering/ErrorImage.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace gipfl\RrdTool\Rendering;
+
+use Exception;
+
+class ErrorImage
+{
+    /** @var Exception|string */
+    protected $error;
+
+    /** @var bool */
+    protected $showStackTrace = false;
+
+    /**
+     * @param Exception|string $error
+     */
+    public function __construct($error)
+    {
+        $this->error = $error;
+    }
+
+    /**
+     * @param bool $show
+     * @return $this
+     */
+    public function showStackTrace($show = true)
+    {
+        $this->showStackTrace = (bool) $show;
+
+        return $this;
+    }
+
+    public function render($width, $height)
+    {
+        $error = $this->error;
+        if ($error instanceof Exception) {
+            $message = $error->getMessage();
+            if ($this->showStackTrace) {
+                $message .= sprintf(
+                    "\nin %s(%s)\n%s\n",
+                    $error->getFile(),
+                    $error->getLine(),
+                    $error->getTraceAsString()
+                );
+            }
+        } else {
+            $message = (string) $error;
+        }
+        $img = \imagecreate($width, $height);
+        $bgColor = \imagecolorallocatealpha($img, 255, 255, 255, 255);
+        $textColor = \imagecolorallocatealpha($img, 200, 100, 100, 0);
+        imagecolortransparent($img, $bgColor);
+
+        $fonts = [4, 2, 1];
+        $lines = null;
+
+        foreach ($fonts as $idx => $font) {
+            $charWidth = \imagefontwidth($font);
+            $charHeight = \imagefontheight($font);
+            $maxChars = floor($width / $charWidth) - 1;
+            $maxLines = floor($height / $charHeight) - 1;
+            $newLines = preg_split("/\n/", \wordwrap($message, $maxChars, "\n", true));
+            if (count($newLines) > $maxLines) {
+                if (! isset($fonts[$idx + 1])) {
+                    // There is no smaller font.
+                    if ($lines === null) {
+                        // We got no fitting size? Keep font.
+                        $lines = $newLines;
+                    } else {
+                        // Otherwise use previous lines and font
+                        $font = $fonts[$idx - 1];
+                    }
+                }
+            } else {
+                $lines = $newLines;
+                break;
+            }
+        }
+
+        foreach ($lines as $nr => $line) {
+            \imagestring(
+                $img,
+                $font,
+                floor($charWidth / 2),
+                $nr * $charHeight + floor($charHeight / 2),
+                $line,
+                $textColor
+            );
+        }
+
+        \ob_start();
+        \imagepng($img);
+        $image = \ob_get_contents();
+        \ob_end_clean();
+
+        return $image;
+    }
+
+    public function renderToJson($width, $height)
+    {
+        return [
+            'graph' => [],
+            'image' => ['width' => $width, 'height' => $height],
+            'value' => [],
+            'raw' =>  "data:image/png;base64,"
+                . base64_encode($this->render($width, $height)),
+            'content-type' => 'image/png',
+            // 'error'
+        ];
+    }
+}

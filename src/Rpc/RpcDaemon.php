18f4fc23636374d06d95c04fad8dc4f0cbfe9fbf
Thomas Gelf <thomas@gelf.net>
2019-02-21 16:24:03 +0100

    Rpc: new namespace

---
+++ src/Rpc/RpcDaemon.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace gipfl\RrdTool\Rpc;
+
+use Icinga\Application\Logger;
+use React\EventLoop\Factory as Loop;
+use React\Socket\ConnectionInterface;
+use React\Socket\Server;
+use SplObjectStorage;
+
+class RpcDaemon
+{
+    /** @var \React\EventLoop\LoopInterface */
+    protected $loop;
+
+    /** @var SplObjectStorage */
+    protected $connections;
+
+    public function run()
+    {
+        $this->loop = Loop::create();
+        $this->listen();
+        $this->loop->run();
+    }
+
+    protected function listen()
+    {
+        $host = '0.0.0.0';
+        $port = 5663;
+        $socket = new Server("$host:$port", $this->loop);
+        $this->connections = new SplObjectStorage();
+        $this->initializeTimers();
+
+        Logger::info("Starting RPC listener on $host:$port");
+        $socket->on('connection', function (ConnectionInterface $connection) use ($host, $port) {
+            $this->connections->attach($connection);
+            Logger::debug('RPC connection from ' . $connection->getRemoteAddress());
+            $session = new RpcSession($connection);
+            $connection->on('close', function () use ($session, $connection) {
+                Logger::debug('Closing RPC session for ' . $connection->getRemoteAddress());
+                unset($session);
+                if ($this->connections) {
+                    $this->connections->detach($connection);
+                }
+            });
+        });
+    }
+
+    protected function initializeTimers()
+    {
+    }
+}
---
+++ src/Rpc/RpcHandler.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace gipfl\RrdTool\Rpc;
+
+use gipfl\Protocol\JsonRpc\Connection;
+use gipfl\Protocol\JsonRpc\Notification;
+use gipfl\Protocol\JsonRpc\PacketHandler;
+use gipfl\Protocol\JsonRpc\Request;
+use gipfl\Protocol\JsonRpc\Response;
+use gipfl\RrdTool\GraphTemplate\TemplateLoader;
+use gipfl\RrdTool\RrdGraph;
+use gipfl\RrdTool\RrdSummary;
+use gipfl\RrdTool\Rrdtool;
+
+class RpcHandler implements PacketHandler
+{
+    /** @var Connection  */
+    protected $connection;
+
+    protected $rrdtool;
+
+    public function __construct(Connection $connection, Rrdtool $rrdtool)
+    {
+        $this->connection = $connection;
+        $this->rrdtool = $rrdtool;
+    }
+
+    public function handle(Notification $packet)
+    {
+        if ($packet instanceof Request) {
+            switch ($packet->getMethod()) {
+                case 'rrd.version':
+                    return 'v0.1.0';
+                case 'rrd.graph':
+                    return $this->prepareGraph($packet);
+                case 'rrd.calculate':
+                    return $this->calculate($packet);
+            }
+        }
+        return Response::forRequest($packet)->setResult('nix');
+    }
+
+    protected function calculate(Request $packet)
+    {
+        $files = $packet->getParam('files');
+        $dsNames = $packet->getParam('ds');
+        $ds = [];
+        foreach ($files as $file) {
+            foreach ($dsNames as $name) {
+                $ds[] = (object)[
+                    'filename'   => $file,
+                    'datasource' => $name,
+                ];
+            }
+        }
+        $start = $packet->getParam('start');
+        $end = $packet->getParam('end');
+
+        $summary = new RrdSummary($this->rrdtool);
+
+        return $summary->summariesForDatasources($ds, $start, $end);
+    }
+
+    protected function prepareGraph(Request $packet)
+    {
+        $graph = new RrdGraph();
+        $graph->setStart($packet->getParam('start'));
+        $graph->setEnd($packet->getParam('end'));
+        $graph->setFormat($packet->getParam('format'));
+        $graph->setWidth($packet->getParam('width'));
+        $graph->setHeight($packet->getParam('height'));
+        $onlyGraph = $packet->getParam('onlyGraph');
+        if ($onlyGraph) {
+            $graph->setOnlyGraph();
+        }
+
+        $template = $packet->getParam('template');
+        $file = $packet->getParam('template');
+
+        $loader = new TemplateLoader();
+        $template = $loader->load($template, $file);
+        $template->applyToGraph($graph);
+        $image = base64_encode($graph->getRaw($this->rrdtool, true));
+
+        return $image;
+    }
+}

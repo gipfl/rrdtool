21e8cc55d788fc12a770e8b294c1c8f051428b6b
Thomas Gelf <thomas@gelf.net>
2019-02-21 22:36:01 +0100

    CpuGraph: show missing space dark

--- src/GraphTemplate/CpuGraph.php
+++ src/GraphTemplate/CpuGraph.php
@@ -11,8 +11,6 @@ class CpuGraph extends Template
         $filename = $this->filename;
 
         $parts = [
-            'user'       => 'F9E962', // user
-            'system'     => 'F9AF62', // sys
             'iowait'     => 'F96266', // iowait
             'softirq'    => 'F962F5', // softirq
             'irq'        => '8362F9', // irq
@@ -20,19 +18,25 @@ class CpuGraph extends Template
             'steal'      => '000000',
             'guest'      => '333333',
             'guest_nice' => 'aaaaaa',
+            'system'     => 'F9AF62', // sys
+            'user'       => 'F9E962', // user
             'idle'       => '44bb77', // idle
-
         ];
+        $defs = [];
 
         $first = true;
         foreach ($parts as $ds => $color) {
             $def = $graph->def($filename, $ds, 'AVERAGE');
-            if ($ds === 'idle') {
-                $def = $graph->cdef("$def,100,MAXNAN");
-            }
+            $defs[$ds] = $def;
             $graph->area($def, $color, ! $first);
             $first = false;
         }
+
+        // 100 - sum(all values), 0 if negative:
+        $cdef = '100,' . implode(',', array_values($defs)) . str_repeat(',+', count($defs) - 1)
+            . ',-,0,MAX';
+        $cdef = $graph->cdef($cdef);
+        $graph->area($cdef, '00000033', true);
 
         $graph->setLowerLimit(0)->setUpperLimit(100)->setRigid();
     }

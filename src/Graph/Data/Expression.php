c86cc0ffe206f2e819998d70c4df3f7e048e2d17
Thomas Gelf <thomas@gelf.net>
2019-11-29 15:13:03 +0100

    Data: add C/VDef-related documentation

--- src/Graph/Data/Expression.php
+++ src/Graph/Data/Expression.php
@@ -2,6 +2,35 @@
 
 namespace gipfl\RrdTool\Graph\Data;
 
+/**
+ * Either a CDEF or a VDEF
+ *
+ * About CDEF versus VDEF (man rrdgraph_data)
+ * ------------------------------------------
+ * At some point in processing, RRDtool has gathered an array of rates ready to
+ * display.
+ *
+ * CDEF works on such an array. For example, CDEF:new=ds0,8,* would multiply
+ * each of the array members by eight (probably transforming bytes into bits).
+ * The result is an array containing the new values.
+ *
+ * VDEF also works on such an array but in a different way. For example,
+ * VDEF:max=ds0,MAXIMUM would scan each of the array members and store the
+ * maximum value.
+ *
+ * ## When do you use VDEF versus CDEF?
+ *
+ * Use CDEF to transform your data prior to graphing. In the above example, we'd
+ * use a CDEF to transform bytes to bits before graphing the bits.
+ *
+ * You use a VDEF if you want max(1,5,3,2,4) to return five which would be
+ * displayed in the graph's legend (to answer, what was the maximum value during
+ * the graph period).
+ *
+ * If you want to apply 'complex' operations to the result of a VDEF you have
+ * to use a CDEF again since VDEFs only look like RPN expressions, they aren't
+ * really.
+ */
 class Expression extends Definition
 {
     protected $tag;
@@ -25,7 +54,7 @@ class Expression extends Definition
 
     /**
      * @param string $expression
-     * @return Expression
+     * @return $this
      */
     public function setExpression($expression)
     {
--- src/Graph/Data/VDef.php
+++ src/Graph/Data/VDef.php
@@ -3,7 +3,24 @@
 namespace gipfl\RrdTool\Graph\Data;
 
 /**
- * Synopsis:
+ * Calculate an aggregated single value
+ *
+ * man rrdgraph_data
+ * -----------------
+ * This command returns a value and/or a time according to the RPN statements
+ * used. The resulting vname will, depending on the functions used, have a
+ * value and a time component. When you use this vname in another RPN expression,
+ * you are effectively inserting its value just as if you had put a number at
+ * that place. The variable can also be used in the various graph and print
+ * elements.
+ *
+ * Example: VDEF:avg=mydata,AVERAGE
+ *
+ * Note that currently only aggregation functions work in VDEF rpn expressions.
+ * Patches to change this are welcome.
+ *
+ * Synopsis
+ * --------
  * VDEF:vname=RPN expression
  */
 class VDef extends Expression

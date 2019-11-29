c8bfe5f4e6d784aece5bd09d31a748459f0aa361
Thomas Gelf <thomas@gelf.net>
2019-11-29 11:52:46 +0100

    StackedGraph: generic base, simplify CpuGraph

--- src/GraphTemplate/CpuGraph.php
+++ src/GraphTemplate/CpuGraph.php
@@ -2,45 +2,18 @@
 
 namespace gipfl\RrdTool\GraphTemplate;
 
-use gipfl\RrdTool\Graph\Area;
-use gipfl\RrdTool\RrdGraph;
-
-class CpuGraph extends Template
+class CpuGraph extends StackedGraph
 {
-    public function applyToGraph(RrdGraph $graph)
-    {
-        $filename = $this->filename;
-
-        $parts = [
-            'iowait'     => '#F96266',
-            'softirq'    => '#F962F5',
-            'irq'        => '#8362F9',
-            'nice'       => '#D48823',
-            'steal'      => '#000000',
-            'guest'      => '#333333',
-            'guest_nice' => '#aaaaaa',
-            'system'     => '#F9AF62',
-            'user'       => '#F9E962',
-            'idle'       => '#44bb77',
-        ];
-        $defs = [];
-
-        $first = true;
-        foreach ($parts as $ds => $color) {
-            $def = $graph->def($filename, $ds, 'AVERAGE');
-            $defs[$ds] = $def;
-            $area = new Area($def, $color);
-            $area->setStack(! $first);
-            $graph->add($area);
-            $first = false;
-        }
-
-        // 100 - sum(all values), 0 if negative:
-        $cdef = '100,' . implode(',', array_values($defs)) . str_repeat(',+', count($defs) - 1)
-            . ',-,0,MAX';
-        $cdef = $graph->cdef($cdef);
-        $graph->area($cdef, '00000033', true);
-
-        $graph->setLowerLimit(0)->setUpperLimit(100)->setRigid();
-    }
+    protected $parts = [
+        'iowait'     => '#F96266',
+        'softirq'    => '#F962F5',
+        'irq'        => '#8362F9',
+        'nice'       => '#D48823',
+        'steal'      => '#000000',
+        'guest'      => '#333333',
+        'guest_nice' => '#aaaaaa',
+        'system'     => '#F9AF62',
+        'user'       => '#F9E962',
+        'idle'       => '#44bb77',
+    ];
 }
---
+++ src/GraphTemplate/StackedGraph.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace gipfl\RrdTool\GraphTemplate;
+
+use gipfl\RrdTool\Graph\Area;
+use gipfl\RrdTool\RrdGraph;
+
+abstract class StackedGraph extends Template
+{
+    protected $parts = [];
+
+    public function applyToGraph(RrdGraph $graph)
+    {
+        $filename = $this->filename;
+
+        $parts = $this->parts;
+        $defs = [];
+
+        foreach ($this->getArrayParam('disableDatasources') as $dsName) {
+            unset($parts[$dsName]);
+        }
+
+        $first = true;
+        $shift = $this->getParam('shift');
+        $max = 100;
+
+        foreach ($parts as $ds => $color) {
+            $def = $graph->def($filename, $ds, 'AVERAGE');
+            $defs[$ds] = $def;
+
+            // Shift test
+            if ($shift !== null && $first) {
+                $cdef = $graph->cdef("$def,0,*,$shift,+", 'shift');
+                $graph->add(new Area($cdef, '00000000'));
+                $first = false;
+            }
+            // Shift end
+
+            $area = new Area($def, $color);
+            $area->setStack(! $first);
+            $graph->add($area);
+            $first = false;
+        }
+
+        // 100 - sum(all values), 0 if negative:
+        $cdef = "$max," . implode(',', array_values($defs))
+            . str_repeat(',+', count($defs) - 1)
+            . ',-,0,MAX';
+        $cdef = $graph->cdef($cdef);
+        $graph->area($cdef, '00000033', true);
+
+        $graph->setLowerLimit(min($shift, 0))->setUpperLimit(max($max + $shift, 0))->setRigid();
+    }
+}

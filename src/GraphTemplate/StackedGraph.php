50ebcfe5b58cf600c1946cd1e5c06bbed4bf7090
Thomas Gelf <thomas@gelf.net>
2019-12-11 17:06:16 +0100

    StackedGraph: new settings

--- src/GraphTemplate/StackedGraph.php
+++ src/GraphTemplate/StackedGraph.php
@@ -9,11 +9,16 @@ abstract class StackedGraph extends Template
 {
     protected $parts = [];
 
+    protected function getParts()
+    {
+        return $this->parts;
+    }
+
     public function applyToGraph(RrdGraph $graph)
     {
         $filename = $this->filename;
 
-        $parts = $this->parts;
+        $parts = $this->getParts();
         $defs = [];
 
         foreach ($this->getArrayParam('disableDatasources') as $dsName) {
@@ -22,7 +27,13 @@ abstract class StackedGraph extends Template
 
         $first = true;
         $shift = $this->getParam('shift');
+        $min = null;
+        $max = null;
+        $min = 0;
         $max = 100;
+
+        // This is to show what is missing to reach Max in a Stack
+        $showMissingOnTop = $this->getParam('showMissingOnTop');
 
         foreach ($parts as $ds => $color) {
             $def = $graph->def($filename, $ds, 'AVERAGE');
@@ -42,13 +53,21 @@ abstract class StackedGraph extends Template
             $first = false;
         }
 
-        // 100 - sum(all values), 0 if negative:
-        $cdef = "$max," . implode(',', array_values($defs))
-            . str_repeat(',+', count($defs) - 1)
-            . ',-,0,MAX';
-        $cdef = $graph->cdef($cdef);
-        $graph->area($cdef, '00000033', true);
+        if ($max !== null) {
+            $graph->setUpperLimit(max($max + $shift, 0))->setRigid();
 
-        $graph->setLowerLimit(min($shift, 0))->setUpperLimit(max($max + $shift, 0))->setRigid();
+            if ($showMissingOnTop) {
+                // 100 - sum(all values), 0 if negative:
+                $cdef = "$max," . implode(',', array_values($defs))
+                    . str_repeat(',+', count($defs) - 1)
+                    . ',-,0,MAX';
+                $cdef = $graph->cdef($cdef);
+                // This colors the missing part:
+                $graph->area($cdef, '00000033', true);
+            }
+        }
+        if ($min !== null) {
+            $graph->setLowerLimit(min($shift, 0))->setRigid();
+        }
     }
 }

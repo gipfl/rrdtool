49ce61f2d8b58f1ebb839b2fd82b0c56138ae832
Thomas Gelf <thomas@gelf.net>
2019-12-12 18:16:25 +0100

    RpcHandler: implement a bunch of new commands

--- src/Rpc/RpcHandler.php
+++ src/Rpc/RpcHandler.php
@@ -6,21 +6,24 @@ use gipfl\Protocol\JsonRpc\Error;
 use gipfl\Protocol\JsonRpc\Notification;
 use gipfl\Protocol\JsonRpc\PacketHandler;
 use gipfl\Protocol\JsonRpc\Request;
-use gipfl\Protocol\JsonRpc\Response;
-use gipfl\RrdTool\Graph\Instruction\Shift;
-use gipfl\RrdTool\GraphTemplate\TemplateLoader;
-use gipfl\RrdTool\RrdGraph;
+use gipfl\RrdTool\RrdCached\Client;
 use gipfl\RrdTool\RrdGraphInfo;
 use gipfl\RrdTool\RrdSummary;
 use gipfl\RrdTool\Rrdtool;
+use function React\Promise\all;
+use function React\Promise\resolve;
 
 class RpcHandler implements PacketHandler
 {
     protected $rrdtool;
 
-    public function __construct(Rrdtool $rrdtool)
+    /** @var Client */
+    protected $client;
+
+    public function __construct(Rrdtool $rrdtool, Client $client)
     {
         $this->rrdtool = $rrdtool;
+        $this->client = $client;
     }
 
     public function handle(Notification $packet)
@@ -35,8 +38,42 @@ class RpcHandler implements PacketHandler
                     return $this->recreate($packet);
                 case 'rrdtool.tune':
                     return $this->tune($packet);
+                case 'rrdtool.flush':
+                    return $this->client->flush($packet->getParam('file'));
+                case 'rrdtool.forget':
+                    return $this->client->forget($packet->getParam('file'));
+                case 'rrdtool.flushAndForget':
+                    return $this->client->flushAndForget($packet->getParam('file'));
+                case 'rrdtool.info':
+                    return $this->client->info($packet->getParam('file'));
+                case 'rrdtool.rawinfo':
+                    return $this->client->rawInfo($packet->getParam('file'));
+                case 'rrdtool.pending':
+                    return $this->client->pending($packet->getParam('file'));
+//                case 'rrdtool.last':
+//                    return $this->client->last($packet->getParam('file'));
+                case 'rrdtool.first':
+                    return $this->client->first($packet->getParam('file'), $packet->getParam('rra'));
                 case 'rrdtool.calculate':
                     return $this->calculate($packet);
+                case 'rrdtool.last':
+                    return $this->last($packet);
+                case 'rrdtool.listCommands':
+                    return $this->client->listAvailableCommands();
+                case 'rrdtool.hasCommand':
+                    return $this->client->hasCommand($packet->getParam('command'));
+                case 'rrdtool.listRecursive':
+                    return $this->client->hasCommand('LIST')->then(function ($hasList) {
+                        if ($hasList) {
+                            return $this->client->listRecursive();
+                        } else {
+                            $basedir = $this->rrdtool->getBasedir();
+                            $prefixLen = \strlen($basedir) + 1;
+                            return resolve(\array_map(static function ($file) use ($prefixLen) {
+                                return \substr($file, $prefixLen);
+                            }, \glob($basedir . '/**/*.rrd')));
+                        }
+                    });
                 default:
                     return new Error(Error::METHOD_NOT_FOUND);
             }
@@ -87,6 +124,25 @@ class RpcHandler implements PacketHandler
         RrdGraphInfo::appendImageToProps($props, $imageString, $format);
 
         return $props;
+    }
+
+    protected function last(Request $packet)
+    {
+        if ($file = $packet->getParam('file')) {
+            return $this->client->last($file);
+        } else {
+            $files = $packet->getParam('files');
+            $result = [];
+            foreach ($files as $file) {
+                $result[$file] = $this->client->last("$file.rrd")->then(function ($result) {
+                    var_dump('Success', $result);
+                }, function ($e) {
+                    var_dump('Err', $e);
+                });
+            }
+
+            return all($result);
+        }
     }
 
     protected function calculate(Request $packet)

5f557c09cf50455d4dae3a6a49e9f05abe351c49
Thomas Gelf <thomas@gelf.net>
2019-12-12 12:42:39 +0100

    CpuGraph: more variants, prepare new features

--- src/GraphTemplate/CpuGraph.php
+++ src/GraphTemplate/CpuGraph.php
@@ -2,18 +2,90 @@
 
 namespace gipfl\RrdTool\GraphTemplate;
 
+use gipfl\RrdTool\RrdGraph;
+
 class CpuGraph extends StackedGraph
 {
     protected $parts = [
-        'iowait'     => '#F96266',
-        'softirq'    => '#F962F5',
-        'irq'        => '#8362F9',
-        'nice'       => '#D48823',
-        'steal'      => '#000000',
-        'guest'      => '#333333',
-        'guest_nice' => '#aaaaaa',
-        'system'     => '#F9AF62',
-        'user'       => '#F9E962',
-        'idle'       => '#44bb77',
+        'iowait'     => '#F96266cc',
+        'softirq'    => '#F962F5cc',
+        'irq'        => '#8362F9cc',
+        'nice'       => '#D48823cc',
+        'steal'      => '#000000cc',
+        'guest'      => '#333333cc',
+        'guest_nice' => '#aaaaaacc',
+        'system'     => '#F9AF62cc',
+        'user'       => '#F9E962cc',
+        'idle'       => '#44bb77cc',
     ];
+
+    protected $whiteParts = [
+        'iowait'     => '#FFFFFF55',
+        'softirq'    => '#FFFFFF66',
+        'irq'        => '#FFFFFF77',
+        'nice'       => '#FFFFFF88',
+        'steal'      => '#FFFFFF99',
+        'guest'      => '#FFFFFFAA',
+        'guest_nice' => '#FFFFFFBB',
+        'system'     => '#FFFFFFCC',
+        'user'       => '#FFFFFFDD',
+        'idle'       => '#ffffff00',
+    ];
+
+    protected $blackParts = [
+        'iowait'     => '#00000055',
+        'softirq'    => '#00000066',
+        'irq'        => '#00000077',
+        'nice'       => '#00000088',
+        'steal'      => '#00000099',
+        'guest'      => '#000000AA',
+        'guest_nice' => '#000000BB',
+        'system'     => '#000000CC',
+        'user'       => '#000000DD',
+        'idle'       => '#00000000',
+    ];
+
+    protected function sum(RrdGraph $graph, $defs, $preferredAlias)
+    {
+        return $graph->cdef(
+            \implode(',', $defs) . str_repeat(',+', count($defs) - 1),
+            $preferredAlias
+        );
+    }
+
+    protected function sumNan(RrdGraph $graph, $defs, $preferredAlias)
+    {
+        return $graph->cdef(
+            \implode(',', $defs) . str_repeat(',ADDNAN', count($defs) - 1),
+            $preferredAlias
+        );
+    }
+
+    public function applyToGraph(RrdGraph $graph)
+    {
+        parent::applyToGraph($graph);
+
+        $sum = $this->sum($graph, [
+            'def_average_iowait',
+            'def_average_softirq',
+            'def_average_irq',
+            'def_average_nice',
+            'def_average_steal',
+            'def_average_guest',
+            'def_average_guest_nice',
+            'def_average_system',
+            'def_average_user',
+        ], 'total');
+    }
+
+    protected function getParts()
+    {
+        if ($this->getParam('onlyWhite')) {
+            return $this->whiteParts;
+        } elseif ($this->getParam('onlyBlack')) {
+            return $this->blackParts;
+        } else {
+            return $this->parts;
+        }
+    }
 }

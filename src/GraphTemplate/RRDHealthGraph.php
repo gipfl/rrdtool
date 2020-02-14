fa632e289c52f3c29da263f7dce0776b39e55446
Thomas Gelf <thomas@gelf.net>
2020-02-14 11:59:31 +0100

    RRDHealthGraph: allow to disable parts
    Should this be here?

--- src/GraphTemplate/RRDHealthGraph.php
+++ src/GraphTemplate/RRDHealthGraph.php
@@ -9,8 +9,16 @@ class RRDHealthGraph extends Template
     public function applyToGraph(RrdGraph $graph)
     {
         $filename = $this->filename;
-        $this->simpleSmoke($graph, $filename, 'succeeded', '#44bb77');
-        $this->simpleSmoke($graph, $filename, 'invalid', '#F96266');
-        $this->simpleSmoke($graph, $filename, 'deferred', '#7A64C8', -1);
+        $disabled = \array_map('strtolower', $this->getArrayParam('disableDatasources'));
+
+        if (! in_array('succeeded', $disabled)) {
+            $this->simpleSmoke($graph, $filename, 'succeeded', '#44bb77');
+        }
+        if (! in_array('invalid', $disabled)) {
+            $this->simpleSmoke($graph, $filename, 'invalid', '#F96266');
+        }
+        if (! in_array('deferred', $disabled)) {
+            $this->simpleSmoke($graph, $filename, 'deferred', '#7A64C8', -1);
+        }
     }
 }

881656a801c081807b39fa6018b6681099ab2453
Thomas Gelf <thomas@gelf.net>
2022-07-29 18:26:47 +0200

    DsTest: test JSON serialization and mapped name

--- tests/DsTest.php
+++ tests/DsTest.php
@@ -2,6 +2,7 @@
 
 namespace gipfl\Tests\RrdTool;
 
+use gipfl\Json\JsonString;
 use gipfl\RrdTool\Ds;
 use PHPUnit\Framework\TestCase;
 
@@ -17,5 +18,46 @@ class DsTest extends TestCase
         $this->assertEquals('DS:temperature:GAUGE:8640:U:10000', $ds->toString());
         $ds = new Ds('temperature', 'GAUGE', 8640, -371, 10000);
         $this->assertEquals('DS:temperature:GAUGE:8640:-371:10000', $ds->toString());
+    }
+
+    public function testAliasCanBeRetrieved()
+    {
+        $ds = new Ds('temperature', 'GAUGE', 8640, null, null, null, 'An alias');
+        $this->assertEquals('An alias', $ds->getAlias());
+    }
+
+    public function testAliasDoesNotInfluenceRendering()
+    {
+        $ds = new Ds('temperature', 'GAUGE', 8640);
+        $ds->setAlias('An alias');
+        $this->assertEquals('DS:temperature:GAUGE:8640:U:U', $ds->toString());
+    }
+
+    public function testParsesAndRendersMappedName()
+    {
+        $ds = new Ds('temperature', 'GAUGE', 8640, -371, 10000, 'new-temp');
+        $this->assertEquals('DS:temperature=new-temp:GAUGE:8640:-371:10000', $ds->toString());
+    }
+
+    public function testCanBeSerializedToJson()
+    {
+        $ds = new Ds('temperature', 'GAUGE', 8640, -371, 10000);
+        $json = '{"name":"temperature","type":"GAUGE","heartbeat":8640,"min":-371,"max":10000,"alias":null,'
+            . '"mappedName":null}';
+        $this->assertEquals($json, JsonString::encode($ds));
+    }
+
+    public function testRestoresFromJson()
+    {
+        $json = '{"name":"temperature","type":"GAUGE","heartbeat":8640,"min":-371,"max":10000,"alias":null,'
+            . '"mappedName":null}';
+        $ds = Ds::fromSerialization(JsonString::decode($json));
+        $this->assertEquals('temperature', $ds->getName());
+        $this->assertEquals('GAUGE', $ds->getType());
+        $this->assertEquals(8640, $ds->getHeartbeat());
+        $this->assertEquals(-371, $ds->getMin());
+        $this->assertEquals(10000, $ds->getMax());
+        $this->assertEquals(null, $ds->getAlias());
+        $this->assertEquals(null, $ds->getMappedName());
     }
 }
